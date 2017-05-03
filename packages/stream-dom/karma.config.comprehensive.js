f71dfe55bca5468a647957f032c84cf4f499535b
Brandon Payton <brandon@happycode.net>
2017-05-02 22:22:15 -0700

    Update test browsers and platforms

--- packages/stream-dom/karma.config.comprehensive.js
+++ packages/stream-dom/karma.config.comprehensive.js
@@ -5,24 +5,12 @@ const customLaunchers = {
     base: 'SauceLabs',
     browserName: 'chrome',
     platform: 'Windows 7',
-    version: '35'
+    version: '57'
   },
   sl_firefox: {
     base: 'SauceLabs',
     browserName: 'firefox',
-    version: '48'
-  },
-  sl_ie_9: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 7',
-    version: '9'
-  },
-  sl_ie_10: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 7',
-    version: '10'
+    version: '52.0'
   },
   sl_ie_11: {
     base: 'SauceLabs',
@@ -30,15 +18,21 @@ const customLaunchers = {
     platform: 'Windows 7',
     version: '11'
   },
+  sl_edge_14: {
+    base: 'SauceLabs',
+    browserName: 'MicrosoftEdge',
+    platform: 'Windows 10',
+    version: '14.14393'
+  },
   sl_safari: {
     base: 'SauceLabs',
     browserName: 'safari',
-    platform: 'OS X 10.11',
-    version: '9.0'
+    platform: 'macOS 10.12',
+    version: '10.0'
   }
 }
 
-module.exports = function(config) {
+module.exports = function (config) {
   const baseConfig = createBaseConfig(config)
 
   config.set(Object.assign({}, baseConfig, {
